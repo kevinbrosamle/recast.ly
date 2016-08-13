a6ce084aa387e6a57114c15435c2850a990daf8a
kevinbrosamle <kevin.brosamle@gmail.com>
2016-08-12 20:56:33 -0700

    Complete stuff for Friday

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,55 +1,17 @@
-// var App = () => (
-//   <div>
-//     <Nav />
-//     <div className="col-md-7">
-//       <VideoPlayer />
-//     </div>
-//     <div className="col-md-5">
-//       <VideoList videos={window.exampleVideoData}/>
-//     </div>
-//   </div>
-// );
 
 var defaultVideo = {
-  kind: 'youtube#searchResult',
-  etag: 'abQHWywil_AkNqdqji7_FqiK-u4/Ykxo_CqKu8F8kcm-iNgL332gQTY',
   id: {
-    kind: 'youtube#video',
-    videoId: '4ZAEBxGipoA'
+    videoId: 'dQw4w9WgXcQ'
   },
   snippet: {
-    publishedAt: '2015-08-02T20:52:58.000Z',
-    channelId: 'UCJbPGzawDH1njbqV-D5HqKw',
-    title: 'React JS Tutorial for Beginners - 1 - Introduction',
-    description: 'My website - https://www.thenewboston.com/videos.php Have questions about the tutorial or React? Ask them here ...',
-    thumbnails: {
-      default: {
-        url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/default.jpg',
-        width: 120,
-        height: 90
-      },
-      medium: {
-        url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/mqdefault.jpg',
-        width: 320,
-        height: 180
-      },
-      high: {
-        url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/hqdefault.jpg',
-        width: 480,
-        height: 360
-      }
-    },
-    channelTitle: 'thenewboston',
-    liveBroadcastContent: 'none'
+    title: 'Rick Astley - Never Gonna Give You Up',
+    description: 'Rick Astley - Never Gonna Give You Up (Official Music Video)'
   }
 };
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-
-    // }
   }
   render() {
     return (
@@ -68,16 +30,3 @@ class App extends React.Component {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
-
-    // return (
-    //   <div>
-    //     <Nav />
-    //     <div className="col-md-7">
-    //       <VideoPlayer videos={window.exampleVideoData}/>
-    //     </div>
-    //     <div className="col-md-5">
-    //       <VideoList videos={window.exampleVideoData}/>
-    //     </div>
-    //   </div>
-    // );
-
