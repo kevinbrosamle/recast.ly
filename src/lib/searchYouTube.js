413bb8ac17181a8cfbfadf0ddfd3602714cc13f3
kevinbrosamle <kevin.brosamle@gmail.com>
2016-08-13 16:20:41 -0700

    Start ajax call

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,17 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  // ajax
+  // url
+  // in success push the results to an array
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    dataType: 'json',
+    success: function(videos) {
+      console.log('ajax working');
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
+// option = {query: 'string', max: 5, key:YOUTUBE_API_KEY}
